169655bbd673e8cc946933b700595afc43ec1193
Willy <willy548798@gmail.com>
2025-10-28 21:55:30 +0800

    Initial Commit

---
+++ NEWSAPI_SProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEWSAPI_SProject
+//
+//  Created by Willy Hsu on 2025/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
